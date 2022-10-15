2846bae3adf544485a895e8aae25438d3f5f3040
ANSAB UDDIN <ansabuddin0346@gmail.com>
2022-10-15 06:11:42 -0700

    neew

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 const con = mysql.createConnection({
     host: '162.144.5.104',
     user: 'webliion_nextmar',
+    port:3306,
     password: 'OcRE409;rTvM',
     database: "webliion_marketplace"
     // host: 'localhost',
